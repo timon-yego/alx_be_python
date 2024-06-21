6ca287554490ee20c11756715a6eb469db5e4418
timon-yego <kipyegotimothy99@gmail.com>
2024-06-21 14:46:54 +0300

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+#Assign specific values to the variables
+number1 = 10
+number2 = 5
+
+#perform arithmetic operations
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+#print the results 
+print(f"Addition:{number1} + {number2} = {addition_result}")
+print(f"Subtraction:{number1} + {number2} = {subtraction_result}")
+print(f"Multiplication:{number1} + {number2} = {multiplication_result}")
+
