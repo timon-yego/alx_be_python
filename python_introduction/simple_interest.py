b3a95ca64c10149d601ed1d80181be0bd13b1704
timon-yego <kipyegotimothy99@gmail.com>
2024-06-21 16:12:20 +0300

    simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,12 @@
 # Calculate simple interest
 
 # define the variabls
-P = 1000  # Principal amount (initial investment)
-R = 0.05  # Annual interest rate (as a decimal)
-T = 3     # Time the money is invested for in years
+principal = 1000  # Principal amount (initial investment)
+rate = 0.05  # Annual interest rate (as a decimal)
+time = 3     # Time the money is invested for in years
 
 # Calculate the simple interest
-I = P * R * T
+I = principal * rate * time
 
 # Print the result
-print(f"The simple interest earned on an investment of {P} at an annual interest rate of {R*100}% for {T} years is {I}.")
+print(f"The simple interest earned on an investment of {principal} at an annual interest rate of {rate*100}% for {time} years is {I}.")
