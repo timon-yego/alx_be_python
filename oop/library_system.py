c83f15bc5d5ded9941eb4d67e6b8c09c341bcbbf
timon-yego <kipyegotimothy99@gmail.com>
2024-07-20 02:07:54 +0300

    added library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
     
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -34,6 +34,5 @@ class Library:
     def list_books(self):
         if not self.books:
             print("No books in the library.")
-        else:
-            for book in self.books:
-                print(book)
+        for book in self.books:
+            print(book)
